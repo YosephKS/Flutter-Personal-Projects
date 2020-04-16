0c5183235a9135c5d471ff2fa156af1aae2e635c
YosephKS <yosephkurniasoenggoro@gmail.com>
2020-04-16 13:20:35 +0700

    Building I am Rich App

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:hello_world/main.dart';
+import 'package:I_am_Rich/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
